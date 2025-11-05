8d6a7469716ee51c836f41186bb4a733cffbc322
seung <kumbung01@naver.com>
2025-11-05 22:16:58 +0900

    fix?

--- src/behaviors/behavior_caps_word_custom.c
+++ src/behaviors/behavior_caps_word_custom.c
@@ -20,9 +20,9 @@
 #include <zmk/hid.h>
 #include <zmk/keymap.h>
 
-extern zmk_mod_flags_t zmk_hid_get_explicit_mods(void);
-extern const struct zmk_event_type zmk_event_zmk_keycode_state_changed;
-extern struct zmk_keycode_state_changed *as_zmk_keycode_state_changed(const zmk_event_t *eh);
+// extern zmk_mod_flags_t zmk_hid_get_explicit_mods(void);
+// extern const struct zmk_event_type zmk_event_zmk_keycode_state_changed;
+// extern struct zmk_keycode_state_changed *as_zmk_keycode_state_changed(const zmk_event_t *eh);
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
